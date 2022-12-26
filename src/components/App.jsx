32b76e30f93f58b57a8a3fcdcf9f9c912d4d3024
Satvik Agrawal <satwikagrawal@gmail.com>
2022-12-26 14:15:53 +0530

    26/12/22

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import Notes from "../notes";
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Note />
+			{Notes.map(x => {
+				return <Note key={x.key} title={x.title} content={x.content} />;
+			})}
 			<Footer />
 		</div>
 	);
